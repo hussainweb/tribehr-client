b4df648b414e34faa17e76bdfbe1449af444c3c2
Hussain Abbas <hussainweb@gmail.com>
2014-12-08 17:00:01 +0000

    Add basic kudos functions within TribeHr\Client

--- src/Client.php
+++ src/Client.php
@@ -92,12 +92,12 @@ class Client {
     /**
      * Prepare and send a request to TribeHR API.
      *
-     * @param $uri
+     * @param string $uri
      * @param string $method
      * @param array $options
      * @return string
      */
-    protected function call($uri, $method = 'GET', $options = []) {
+    public function request($uri, $method = 'GET', $options = []) {
         if (empty($this->subdomain) || empty($this->username) || empty($this->apikey)) {
             throw new \InvalidArgumentException("Access details for API have not been completely specified.");
         }
@@ -110,5 +110,57 @@ class Client {
         $request = $this->httpClient->createRequest($method, $url, $options);
         $response = $this->httpClient->send($request);
         return (string) $response->getBody();
+    }
+
+    /**
+     * Send Kudos to another user.
+     *
+     * @param string|int $receiver_id
+     * @param string $kudos_note
+     * @param array $values
+     */
+    public function sendKudos($receiver_id, $kudos_note, $values = []) {
+        if (!is_numeric($receiver_id)) {
+            $users = array_flip($this->getUserEmails());
+            $receiver_id = $users[$receiver_id];
+        }
+
+        $data = array(
+          'note' => $kudos_note,
+          'user_id' => [$receiver_id],
+          'value_id' => $values,
+        );
+        $this->request('kudos.xml', 'POST', ['body' => $data]);
+    }
+
+    /**
+     * Get all the users keyed by their id.
+     *
+     * @return array
+     */
+    public function getUsers() {
+        $user_list = [];
+
+        $response = $this->request('users.xml');
+        $users = new \SimpleXMLElement($response);
+        foreach ($users->user as $user) {
+            $user_list[(string) $user['id']] = $user;
+        }
+
+        return $user_list;
+    }
+
+    /**
+     * Get all the email addresses of the users keyed by their id.
+     * @return array
+     */
+    public function getUserEmails() {
+        $user_list = [];
+        $users = $this->getUsers();
+        foreach ($users as $user) {
+            $user_list[(string) $user['id']] = (string) $user['email'];
+        }
+
+        return $user_list;
     }
 }
